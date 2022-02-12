acea1e60bf59f6143ca8091dea72c1cfc04e83d3
Ayoub httater <Ayoub.Httater@terabyte-software.com>
2022-02-12 13:26:43 +0100

    Configure Autofac with .net 6

---
+++ src/API/Infrastructure/AutofacModules/ApplicationModule.cs
@@ -0,0 +1,11 @@
+﻿using Autofac;
+
+namespace API.Infrastructure.AutofacModules;
+
+public class ApplicationModule : Module
+{
+    protected override void Load(ContainerBuilder builder)
+    {
+        Console.WriteLine("Not Implemented yet");
+    }
+}
--- src/API/Program.cs
+++ src/API/Program.cs
@@ -1,6 +1,51 @@
-using API;
-using Microsoft.AspNetCore;
+using API.Infrastructure.AutofacModules;
+using API.Infrastructure.Filter;
+using API.Infrastructure.Swagger;
+using Autofac;
+using Autofac.Extensions.DependencyInjection;
 
-var builder = WebHost.CreateDefaultBuilder(args).UseStartup<Startup>();
+var builder = WebApplication.CreateBuilder(args);
 
-builder.Build().Run();
+builder.Services.AddControllers(options => { options.Filters.Add(typeof(HttpGlobalExceptionFilter)); })
+                .AddJsonOptions(options => { options.JsonSerializerOptions.WriteIndented = true; });
+
+// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+builder.Services.AddEndpointsApiExplorer();
+
+builder.Services.AddSwaggerConfiguration();
+
+builder.Host.UseServiceProviderFactory(new AutofacServiceProviderFactory());
+
+// Register services directly with Autofac here. Don't
+// call builder.Populate(), that happens in AutofacServiceProviderFactory.
+builder.Host.ConfigureContainer<ContainerBuilder>(builder =>
+{
+    builder.RegisterModule(new ApplicationModule());
+});
+
+var app = builder.Build();
+
+// Configure the HTTP request pipeline.
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
+
+app.UseCors(c => { c.AllowAnyHeader(); c.AllowAnyMethod(); });
+
+app.UseRouting();
+
+app.UseAuthorization();
+
+app.UseEndpoints(endpoints => endpoints.MapControllers());
+
+app.UseHttpsRedirection();
+
+app.UseStaticFiles();
+
+app.UseAuthentication();
+
+app.UseSwaggerSetup();
+
+app.Run();
